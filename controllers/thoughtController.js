4f1b9a3e83431ce0126dca740663784234ee39a4
lyskz <lyski.zach@icloud.com>
2023-05-27 16:51:10 -0700

    controller structure

---
+++ controllers/thoughtController.js
@@ -0,0 +1,39 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+
+        } catch (err) {
+
+        }
+    },
+    async getSingleThought(req, res) {
+        try {
+
+        } catch (err) {
+
+        }
+    },
+    async createThought(req, res) {
+        try {
+            
+        } catch (err) {
+
+        }
+    },
+    async updateThought(req, res) {
+        try {
+
+        } catch (err) {
+
+        }
+    },
+    async deleteThought(req, res)  {
+        try {
+
+        } catch (err) {
+
+        }
+    },
+};
---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getUsers(req, res) {
+        try {
+
+        } catch (err) {
+
+        }
+    },
+    async getSingleUser(req, res) {
+        try {
+
+        } catch (err) {
+
+        }
+    },
+    async createUser(req, res) {
+        try {
+
+        } catch (err) {
+
+        }
+    },
+    async updateUser(req, res) {
+        try {
+
+        } catch (err) {
+
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+            
+        } catch (err) {
+
+        }
+    },
+};
